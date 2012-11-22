4f154e4282a15fa7a16a500fb06eff5c278f765a
Doug Ireton <doug.ireton@nordstrom.com>
2012-11-21 22:32:01 -0800

    Add case node[platform_family] block for depends

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs git and/or sets up a Git server daemon"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.2"
+version           "1.0.3"
 recipe            "git", "Installs git"
 recipe            "git::server", "Sets up a runit_service for git daemon"
 recipe            "git::source", "Installs git from source"
@@ -15,6 +15,15 @@ end
 
 supports "mac_os_x", ">= 10.6.0"
 
-%w{ build-essential dmg runit yum }.each do |cb|
-  depends cb
+case node['platform_family']
+when 'mac_os_x'
+  depends 'dmg'
+when 'debian'
+  depends 'runit'
+  depends 'build-essential'
+when 'rhel', 'fedora'
+  depends 'yum'
+  depends 'build-essential'
+when 'windows'
+  depends 'windows'
 end
