e7621b8c739972bf71e964eb64b6e6f6858c71ca
Ryan J. Geyer <me@ryangeyer.com>
2013-01-30 13:43:53 -0800

    [COOK-2303] Made export_all optional and added syslog on ubuntu

--- attributes/default.rb
+++ attributes/default.rb
@@ -37,3 +37,4 @@ else
 end
 
 default['git']['server']['base_path'] = "/srv/git"
+default['git']['server']['export_all'] = "true"
--- metadata.rb
+++ metadata.rb
@@ -26,3 +26,10 @@ attribute "git/server/base_path",
   :description => "A directory containing git repositories to be exposed by the git-daemon",
   :default => "/srv/git",
   :recipes => ["git::server"]
+
+attribute "git/server/export_all",
+  :display_name => "Git Daemon Export All",
+  :description => "Adds the --export-all option to the git-daemon parameters, making all repositories publicly readable even if they lack the \"git-daemon-export-ok\" file",
+  :choice => ["true", "false"],
+  :default => "true",
+  :recipes => ["git::server"]
