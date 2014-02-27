788d82d0a99a67cd3729e6dc846c30603dfd61a4
Guilhem Lettron <guilhem.lettron@optiflows.com>
2014-02-27 18:53:34 +0100

    Add LWRP config

---
+++ resources/config.rb
@@ -0,0 +1,10 @@
+actions :set
+default_action :set
+
+attribute :key,   :kind_of => String, :name_attribute => true
+attribute :value, :kind_of => String, :required => true
+attribute :scope, :equal_to => %w(locale global system), :default => 'global'
+attribute :path,  :kind_of => String
+attribute :user,  :kind_of => String
+
+attr_accessor :exists
