a743081aecda318f6c2aaa6658862364d681c0be
Guilhem Lettron <guilhem.lettron@optiflows.com>
2014-02-28 18:12:00 +0100

    Fix env variables to set for git config to work Make it more robust, less complicated Just fine!

--- providers/config.rb
+++ providers/config.rb
@@ -12,6 +12,7 @@ action :set do
       cwd new_resource.path
       user new_resource.user
       group new_resource.user
+      environment cmd_env
       Chef::Log.info "#{ @new_resource } created."
     end
   end
@@ -28,32 +29,17 @@ def load_current_resource
   @current_resource.exists = true if config == new_resource.value
 end
 
-# There is a problem with git config who don't manage good user
-# Even if when set good user in execute resource
-# "git config --#{new_resource.scope}"
-
 def config_cmd
-  cmd = "git config --file #{gitconfig_file}"
-  cmd.concat(" #{new_resource.options}") if new_resource.options
-  cmd
+  "git config --#{new_resource.scope}"
 end
 
-def gitconfig_file
-  case new_resource.scope
-  when 'system'
-    return '/etc/gitconfig'
-  when 'global'
-    Chef::Log.error('Scope global need an user') unless new_resource.user
-    return ::File.join(::Dir.home(new_resource.user), '.gitconfig')
-  when 'local'
-    Chef::Log.error('Scope local need a path') unless new_resource.path
-    return ::File.join(new_resource.path, '.gitconfig')
-  end
+def cmd_env
+  new_resource.user ? { 'USER' => new_resource.user, 'HOME' => ::Dir.home(new_resource.user) } : nil
 end
 
 def config
   cmd = [config_cmd, new_resource.key].join(' ')
-  git_config = Mixlib::ShellOut.new(cmd, :user => new_resource.user, :group => new_resource.user, :cwd => new_resource.path)
+  git_config = Mixlib::ShellOut.new(cmd, :user => new_resource.user, :group => new_resource.user, :cwd => new_resource.path, :env => cmd_env)
   Chef::Log.debug("Current config cmd: #{git_config.inspect}")
   git_config.run_command.stdout.chomp
 end
