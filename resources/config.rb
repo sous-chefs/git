9523b6433f9bde2dfe08a556a442b1803e397405
Guilhem Lettron <guilhem.lettron@optiflows.com>
2014-02-28 11:19:49 +0100

    Fix bad name for local

--- resources/config.rb
+++ resources/config.rb
@@ -3,7 +3,7 @@ default_action :set
 
 attribute :key,   :kind_of => String, :name_attribute => true
 attribute :value, :kind_of => String, :required => true
-attribute :scope, :equal_to => %w(locale global system), :default => 'global'
+attribute :scope, :equal_to => %w(local global system), :default => 'global'
 attribute :path,  :kind_of => String
 attribute :user,  :kind_of => String
 attribute :options, :kind_of => String
