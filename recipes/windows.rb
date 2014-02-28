baf3bf36dee29c5344929b4f02836504d960fd43
Guilhem Lettron <guilhem.lettron@optiflows.com>
2014-02-28 11:50:53 +0100

    Fix rubocop

--- recipes/windows.rb
+++ recipes/windows.rb
@@ -31,14 +31,14 @@ GIT_PATH = "#{ PROGRAM_FILES }\\Git\\Cmd"
 # COOK-3482 - windows_path resource doesn't change the current process
 # environment variables. Therefore, git won't actually be on the PATH
 # until the next chef-client run
-ruby_block "Add Git Path" do
-	block do
-		ENV['PATH'] += ";#{GIT_PATH}"
-	end
-	action :nothing
+ruby_block 'Add Git Path' do
+  block do
+    ENV['PATH'] += ";#{GIT_PATH}"
+  end
+  action :nothing
 end
 
 windows_path GIT_PATH do
   action :add
-  notifies :create, "ruby_block[Add Git Path]", :immediately
+  notifies :create, 'ruby_block[Add Git Path]', :immediately
 end
