eb999eed03f943bce60b9109d7352ef13f440036
Guilhem Lettron <guilhem.lettron@optiflows.com>
2014-02-28 11:28:51 +0100

    Add more tests for git_config

--- test/cookbooks/git_test/recipes/default.rb
+++ test/cookbooks/git_test/recipes/default.rb
@@ -3,9 +3,27 @@ user 'random' do
   home '/home/random'
 end
 
-git_config 'add name to root' do
+git_config 'add name to random' do
   user 'random'
   scope 'global'
   key 'user.name'
-  value 'John Doe'
+  value 'John Doe global'
+end
+
+directory '/home/random/git_repo' do
+  owner 'random'
+end
+
+git_config 'change local path' do
+  user 'random'
+  scope 'local'
+  key 'user.name'
+  value 'John Doe local'
+  path '/home/random/git_repo'
+end
+
+git_config 'change system config' do
+  scope 'system'
+  key 'user.name'
+  value 'John Doe system'
 end
