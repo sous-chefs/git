5e86aeecfe02153468a82506de73c79aef77ffb8
rapenchukd <rapenchuk@linux.com>
2014-12-05 16:11:28 -0500

    Update source.rb

--- recipes/source.rb
+++ recipes/source.rb
@@ -29,6 +29,8 @@ when 'rhel'
     pkgs = %w{ expat-devel gettext-devel curl-devel openssl-devel zlib-devel }
   when 6
     pkgs = %w{ expat-devel gettext-devel libcurl-devel openssl-devel perl-ExtUtils-MakeMaker zlib-devel }
+  when 7
+    pkgs = %w{ expat-devel gettext-devel libcurl-devel openssl-devel perl-ExtUtils-MakeMaker zlib-devel }
   else
     pkgs = %w{ expat-devel gettext-devel curl-devel openssl-devel perl-ExtUtils-MakeMaker zlib-devel } if node['platform'] == 'amazon'
   end
