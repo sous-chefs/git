b9332308e8f330b2f59bf6476549e2a364b7ff70
John Bellone <jbellone@bloomberg.net>
2014-12-08 17:08:46 -0500

    Restructure the ChefSpec test coverage files.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,35 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 
+RSpec.configure do |config|
+  config.color = true
+  config.log_level = :fatal # ignore warnings
+  config.alias_example_group_to :describe_recipe, type: :recipe
+  config.alias_example_group_to :describe_resource, type: :resource
+  config.alias_example_group_to :describe_attribute, type: :attribute
+
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
+  Kernel.srand config.seed
+  config.order = :random
+
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
+
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+    mocks.verify_partial_doubles = true
+  end
+end
+
+RSpec.shared_context 'recipe tests', type: :recipe do
+  let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '12.04').converge(described_recipe) }
+end
+
 at_exit { ChefSpec::Coverage.report! }
