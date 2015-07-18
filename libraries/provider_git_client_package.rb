a469fa1b1d736cd695dc50a1fe865fb789829325
Fletcher Nichol <fnichol@nichol.ca>
2015-07-18 14:14:30 -0600

    Prefer `&&` to `and` in normal conditional logic (style).

--- libraries/provider_git_client_package.rb
+++ libraries/provider_git_client_package.rb
@@ -8,7 +8,7 @@ class Chef
 
         action :install do
           # FIXME: rhel 5
-          include_recipe 'yum-epel' if node['platform_family'] == 'rhel' and node['platform_version'].to_i == 5
+          include_recipe 'yum-epel' if node['platform_family'] == 'rhel' && node['platform_version'].to_i == 5
 
           # Software installation
           package "#{new_resource.name} :create #{parsed_package_name}" do
