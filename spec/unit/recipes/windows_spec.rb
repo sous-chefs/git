8aa6827b8d5adc68a19981264d9a1cfb84f33837
Sean OMeara <sean@chef.io>
2016-01-25 14:46:05 -0500

    updating specs again

--- spec/unit/recipes/windows_spec.rb
+++ spec/unit/recipes/windows_spec.rb
@@ -9,10 +9,4 @@ describe_recipe 'git::windows' do
   end
 
   it { expect(chef_run).to install_git_client('default') }
-  it do
-    expect(chef_run).to install_windows_package(chef_run.node['git']['display_name'])
-      .with(source: chef_run.node['git']['url'])
-      .with(checksum: chef_run.node['git']['checksum'])
-      .with(installer_type: :inno)
-  end
 end
