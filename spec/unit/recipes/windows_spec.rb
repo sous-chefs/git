36eb6921a53b43215cd9246e3ae12c63c5a13289
Sean OMeara <sean@chef.io>
2016-01-25 14:45:06 -0500

    updating specs

--- spec/unit/recipes/windows_spec.rb
+++ spec/unit/recipes/windows_spec.rb
@@ -5,7 +5,7 @@ describe_recipe 'git::windows' do
     ChefSpec::SoloRunner.new(
       platform: 'windows',
       version: '2012',
-      step_into: 'git_client').converge(described_recipe)
+      ).converge(described_recipe)
   end
 
   it { expect(chef_run).to install_git_client('default') }
@@ -14,15 +14,5 @@ describe_recipe 'git::windows' do
       .with(source: chef_run.node['git']['url'])
       .with(checksum: chef_run.node['git']['checksum'])
       .with(installer_type: :inno)
-  end
-
-  it { expect(chef_run).to add_windows_path('C:\Program Files (x86)\Git\Cmd') }
-  it do
-    resource = chef_run.windows_path('C:\Program Files (x86)\Git\Cmd')
-    expect(resource).to notify('ruby_block[Add Git Path]').to(:create).immediately
-  end
-  it do
-    resource = chef_run.ruby_block('Add Git Path')
-    expect(resource).to do_nothing
   end
 end
