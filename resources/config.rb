2042f182534cdccad7adf2b1b825669a71a0e75b
Ming Xie <zmingxie@gmail.com>
2016-05-16 14:19:09 -0400

    Add a new attribute for user group configuration

--- resources/config.rb
+++ resources/config.rb
@@ -6,6 +6,7 @@ attribute :value, kind_of: String, required: true
 attribute :scope, equal_to: %w(local global system), default: 'global'
 attribute :path,  kind_of: String
 attribute :user,  kind_of: String
+attribute :group, kind_of: String
 attribute :options, kind_of: String
 
 attr_accessor :exists
