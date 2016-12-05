3c1b93ead24f78ea38fe74b546143480b14f9c20
Tim Smith <tsmith@chef.io>
2016-12-05 12:22:43 -0800

    Update copyrights
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,9 @@
 #
 # Author:: Jamie Winsor (<jamie@vialstudios.com>)
-# Cookbook Name:: git
+# Cookbook:: git
 # Attributes:: default
 #
-# Copyright 2008-2016, Chef Software, Inc.
+# Copyright:: 2008-2016, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
--- recipes/source.rb
+++ recipes/source.rb
@@ -1,8 +1,8 @@
 #
-# Cookbook Name:: git
+# Cookbook:: git
 # Recipe:: source
 #
-# Copyright 2012, Brian Flad, Fletcher Nichol
+# Copyright:: 2012-2016, Brian Flad, Fletcher Nichol
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
