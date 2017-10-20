ef798bbc2c16f6f6c402b7c5f3f61cff3ac3224c
Jennifer Davis <iennae@gmail.com>
2017-10-20 14:47:54 -0700

    Update source test.
    Signed-off-by: Jennifer Davis <iennae@gmail.com>

--- spec/unit/recipes/source_spec.rb
+++ spec/unit/recipes/source_spec.rb
@@ -4,9 +4,8 @@ require 'spec_helper'
 
 describe_recipe 'git::source' do
   context 'when using include_recipe or adding git::default to the run_list' do
-    it 'installs git_client[default]' do
-      expect(chef_run).to install_git_client('default')
-        .with(provider: Chef::Provider::GitClient::Source)
+    it 'installs git_client_source[default]' do
+      expect(chef_run).to install_git_client_source('default')
     end
   end
 end
