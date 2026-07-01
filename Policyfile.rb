9fe259093b1cb7ab9fe2c2c5657cd4a85d5af3c9
Dan Webb <dan.webb@damacus.io>
2026-07-01 23:04:32 +0100

    chore: migrate to Policyfile
    # Conflicts:
    #	.github/workflows/ci.yml
    #	.github/workflows/conventional-commits.yml
    #	.github/workflows/copilot-setup-steps.yml
    #	.github/workflows/prevent-file-change.yml
    #	.github/workflows/release.yml

---
+++ Policyfile.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+name 'git'
+
+default_source :supermarket
+
+run_list 'test::default'
+
+cookbook 'git', path: '.'
+cookbook 'test', path: './test/cookbooks/test'
+
+Dir.children('./test/cookbooks/test/recipes').grep(/\.rb\z/).sort.each do |recipe|
+  recipe_name = File.basename(recipe, '.rb')
+
+  named_run_list recipe_name.to_sym, "test::#{recipe_name}"
+end
