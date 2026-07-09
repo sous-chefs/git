52b4a7c070863d8cb2ee359806bfe8abd82e3cca
Dan Webb <dan.webb@damacus.io>
2026-07-09 10:47:43 +0100

    fix: migrate to Policyfile and map Kitchen suites (#218)
    * chore: migrate to Policyfile
    
    # Conflicts:
    #	.github/workflows/ci.yml
    #	.github/workflows/conventional-commits.yml
    #	.github/workflows/copilot-setup-steps.yml
    #	.github/workflows/prevent-file-change.yml
    #	.github/workflows/release.yml
    
    * ci: use Policyfile for Copilot setup
    
    * fix: map Kitchen suites to Policyfile run lists

--- test/cookbooks/test/recipes/source.rb
+++ test/cookbooks/test/recipes/source.rb
@@ -2,6 +2,9 @@
 
 apt_update 'update apt cache' if platform_family?('debian')
 
+package 'ca-certificates'
+
 git_client 'source' do
+  source_url 'http://mirrors.edge.kernel.org/pub/software/scm/git/git-2.54.0.tar.gz'
   action :install_from_source
 end
